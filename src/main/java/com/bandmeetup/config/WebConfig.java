8c0a2fad737d8ea4d2599a75d4c4066cf40219f0
Stephen Cook <sjc5897@rit.edu>
2020-10-21 15:38:46 -0400

    Needed to get an WebMVC configurer created to more resolve templating and css issues

---
+++ src/main/java/com/bandmeetup/config/WebConfig.java
@@ -0,0 +1,73 @@
+package com.bandmeetup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+/**
+ * Class that assists in the configuration of MVC, it also is used to configure thymeleaf more.
+ * Language: Java 13
+ * Framework: Spring
+ * Author: Stephen Cook <sjc5897@rit.edu>
+ * Created: 10/21/20
+ */
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * Registration of controllers, done via this tutorial: https://www.baeldung.com/spring-mvc-tutorial'
+     * Note the tutorial uses jsp, not thymeleaf
+     * @param registry A ViewController Registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("index");
+    }
+
+    /**
+     * Sets up a the systems templating engine: https://www.baeldung.com/spring-template-engines#2-spring-configuration
+     * @return SpringTemplateEngine a new template engine
+     */
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine =  new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+        return templateEngine;
+    }
+
+    /**
+     * Configues a Thymeleaf Template Resolver to our file structure
+     * @return
+     */
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        return templateResolver;
+    }
+    @Bean
+    /**
+     * Creates a Thymeleaf view resolver
+     */
+    public ThymeleafViewResolver thymeleafViewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+    /**
+     * Configures our resource location for css files.
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**", "/css/**")
+                .addResourceLocations("/WEB-INF/resources/", "/WEB-INF/css/");
+    }
+}
