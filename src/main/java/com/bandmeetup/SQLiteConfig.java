20c1a5ffe00e0cf3ef2ffb005cd5181bfd703c0f
Sultan <seetoo0oo@hotmail.com>
2020-10-23 00:49:39 -0400

    update DB  and login funcation to verify auth

--- src/main/java/com/bandmeetup/SQLiteConfig.java
+++ src/main/java/com/bandmeetup/SQLiteConfig.java
@@ -1,10 +1,7 @@
 package com.bandmeetup;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLiteConfig {
     static Connection conn = null;
@@ -24,7 +21,7 @@ public class SQLiteConfig {
     public static void insert(String E,String N,String P,String T) throws ClassNotFoundException {
 
 
-        String sql = "INSERT INTO Musician(Email, Name, Password,UserType) VALUES(?,?,?,?)";
+        String sql = "INSERT INTO Musician(Email, FName, Password,UserType) VALUES(?,?,?,?)";
         Class.forName("org.sqlite.JDBC");
         try (Connection conn = connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -39,10 +36,45 @@ public class SQLiteConfig {
             System.out.println(e.getMessage());
         }
     }
+    public static boolean loginAuth(String email,String password) throws ClassNotFoundException {
+
+    // An example where er can verify login
+        boolean flag = false;
+
+        String sql = "select Email, Password from User;";
+        Class.forName("org.sqlite.JDBC");
+        try (Connection conn = connect();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            try {
+                ResultSet results = ps.executeQuery();
+
+                while (results.next()) {
+
+                    String Email = results.getString("Email");
+                    String Password =  results.getString("Password");
+                    System.out.println(Email);
+
+                    if ((email.equals(Email)) && (password.equals(Password))) {
+                        flag = true; }
+                    results.close();
+                }
+            }catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return flag;
+    }
     public static void main(String[] args) throws Exception {
 
 
-        insert("D@d","Devan","123123","musician");
+        //insert("D@2d","Dean","12123","musician");
+        boolean Pass =loginAuth("s@s","123456");
+        System.out.println(Pass);
+
 
 
     }
