53a008a64506b42a96b0935b19a692d5becdde12
Stephen Cook <sjc5897@rit.edu>
2020-10-23 09:09:45 -0400

    Started to set up registration

---
+++ src/main/java/com/bandmeetup/services/RegistrationService.java
@@ -0,0 +1,12 @@
+package com.bandmeetup.services;
+
+import com.bandmeetup.model.User;
+
+public class RegistrationService {
+//    public boolean newAccount(String email, String pw, String type){
+//        //User nuser = new User(email,pw,type);
+//
+//        // TODO persist User
+//    }
+}
+
